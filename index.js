cd44a15c88de9ca7742666d56d929718dc59c310
CyrilDenoyelle <Cyril-denoyelle@hotmail.fr>
2023-08-24 15:14:30 +0200

    add eslint and apply

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-const fs = require("fs");
-const ffmpeg = require("fluent-ffmpeg");
-const mic = require("mic");
-const { Readable } = require("stream");
-const ffmpegPath = require("@ffmpeg-installer/ffmpeg").path;
+const fs = require('fs')
+const ffmpeg = require('fluent-ffmpeg')
+const mic = require('mic')
+const { Readable } = require('stream')
 
-const { OpenAI } = require("openai");
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path
 
-require('dotenv').config();
+const { OpenAI } = require('openai')
 
-const openai = new OpenAI();
+require('dotenv').config()
 
-ffmpeg.setFfmpegPath(ffmpegPath);
+const openai = new OpenAI()
+
+ffmpeg.setFfmpegPath(ffmpegPath)
 
 // Record audio
 function recordAudio(filename) {
@@ -19,49 +20,48 @@ function recordAudio(filename) {
             rate: '16000',
             channels: '1',
             fileType: 'wav',
-            exitOnSilence: 5
-        });
+            exitOnSilence: 5,
+        })
 
-        const micInputStream = micInstance.getAudioStream();
-        const output = fs.createWriteStream(filename);
-        const writable = new Readable().wrap(micInputStream);
+        const micInputStream = micInstance.getAudioStream()
+        const output = fs.createWriteStream(filename)
+        const writable = new Readable().wrap(micInputStream)
 
-        writable.pipe(output);
+        writable.pipe(output)
 
-        micInstance.start();
+        micInstance.start()
 
         micInputStream.on('silence', () => {
-            micInstance.stop();
-            micInputStream
-            console.log('Finished recording by silence');
-            resolve();
-        });
+            micInstance.stop()
+            console.log('Finished recording by silence')
+            resolve()
+        })
 
         micInputStream.on('error', (err) => {
-            reject(err);
-        });
-    });
+            reject(err)
+        })
+    })
 }
 
 // Transcribe audio
 async function transcribeAudio(filename) {
     const transcript = await openai.audio.transcriptions.create({
         file: fs.createReadStream(filename),
-        model: 'whisper-1'
-    });
+        model: 'whisper-1',
+    })
 
-    return transcript.text;
+    return transcript.text
 }
 
-let i = 0;
+let i = 0
 // main function
 async function main() {
-    const audioFilename = `recorded_audios/recorded_audio${i++}.wav`;
-    await recordAudio(audioFilename);
-    const transcription = await transcribeAudio(audioFilename);
-    console.log(`Transcription recorded_audio${i++}:`, transcription);
-    await main();
+    const audioFilename = `recorded_audios/recorded_audio${i}.wav`
+    await recordAudio(audioFilename)
+    const transcription = await transcribeAudio(audioFilename)
+    console.log(`Transcription recorded_audio${i += 1}:`, transcription)
+    await main()
 }
 
-console.log(`Recording... Press Ctrl+C to stop.`);
-main();
+console.log('Recording... Press Ctrl+C to stop.')
+main()
