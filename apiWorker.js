7b035d44010ddabd185f4221860d3a87007437bf
CyrilDenoyelle <Cyril-denoyelle@hotmail.fr>
2023-09-29 21:34:07 +0200

    fix: rework move messages feature in worker to stop messages overlap

--- apiWorker.js
+++ apiWorker.js
@@ -1,12 +1,76 @@
 const { parentPort } = require('worker_threads')
 const fs = require('fs')
 const { OpenAI } = require('openai')
+const gptTokenizer = require('gpt-tokenizer')
+
+gptTokenizer.default.modelName = 'cl100k_base'
+
 const twitchBotGen = require('./auth/twitchBot')
 
 const openai = new OpenAI()
 
-const functions = [
+const openaiBaseSetings = {
+    model: process.env.OPENAI_CHAT_MODEL,
+    max_tokens: parseInt(process.env.OPENAI_MAX_TOKENS_BY_MESSAGE, 10),
+    temperature: parseInt(process.env.OPENAI_TEMPERATURE, 10),
+}
+
+const rolesNames = {
+    user: process.env.PROMPT_USER_NAME,
+    assistant: process.env.PROMPT_ASSISTANT_NAME,
+}
+
+const twitchChatUsers = []
+const twitchChatUserString = async () => {
+    if (twitchChatUsers.length === 0) return ''
+    if (twitchChatUsers.length === 1) return `L'utilisateur: ${twitchChatUsers[0]} est dans le chat twitch.`
+    return `Les utilisateurs: ${twitchChatUsers.slice(0, -1).join(', ')} et ${twitchChatUsers.slice(-1)} sont dans le chat twitch.`
+}
+
+const systemMessages = () => [
     {
+        role: 'system',
+        content: `tu es l'assistant, tu te nommes ${rolesNames.assistant} et tu ne réponds qu'en ton nom.
+Le pseudo de l'utilisateur principal est ${rolesNames.user}.
+L'assistant fait des réponses courtes et précises.
+Il y a toujours le nom de l'utilisateur qui s'exprime devant les messages.
+${twitchChatUserString()}`,
+    },
+    {
+        role: 'user',
+        content: `Dit moi ${rolesNames.assistant} comment ça va ?`,
+    },
+    {
+        role: 'assistant',
+        content: `${rolesNames.assistant}: Salut ${rolesNames.user}, ça va très bien, et vous ?`,
+    },
+    {
+        role: 'user',
+        content: `(twitch chat) Xx_dark_sasuke_xX: "Bonjour ${rolesNames.assistant} !"`,
+    },
+    {
+        role: 'assistant',
+        content: `${rolesNames.assistant}: Salut Xx_dark_sasuke_xX, ça va super, et toi ?`,
+    },
+]
+
+const messages = []
+const messagesPush = (message) => {
+    messages.push(message)
+
+    // shift messages until total tokens is less than process.env.OPENAI_API_MAX_CHAT_TOTAL_TOKEN
+    while (gptTokenizer.isWithinTokenLimit(
+        [...systemMessages(), ...messages]
+            .map((m) => `${m.role}: "${m.content}"\n`)
+            .join(''),
+        parseInt(process.env.OPENAI_API_MAX_CHAT_TOTAL_TOKEN, 10),
+    ) === false) {
+        messages.shift()
+    }
+}
+
+const functions = {
+    addMarker: {
         canRun: ['user'],
         name: 'addMarker',
         description: 'ajoute un marqueur',
@@ -14,29 +78,27 @@ const functions = [
             type: 'object',
             properties: {},
         },
+        f: () => console.log('adding marker'),
     },
-]
-
-const openaiBaseSetings = {
-    model: process.env.OPENAI_CHAT_MODEL,
-    max_tokens: parseInt(process.env.OPENAI_MAX_TOKENS_BY_MESSAGE, 10),
-    temperature: parseInt(process.env.OPENAI_TEMPERATURE, 10),
 };
 
 (async () => {
     const twitchBot = await twitchBotGen()
 
+    const queue = []
+
     twitchBot.onMessage(async (messageEvent) => {
-        parentPort.postMessage({
-            from: `twitchChat:${messageEvent.userDisplayName}`,
-            message: {
-                role: 'user',
-                content: messageEvent.text,
-            },
+        queue.push({
+            f: 'sendMessage',
+            args: [{
+                from: `twitchChat:${messageEvent.userDisplayName}`,
+                message: {
+                    role: 'user',
+                    content: messageEvent.text,
+                },
+            }],
         })
     })
-
-    const queue = []
 
     const tasks = {
         sendAudioToOpenAi: async (audioFilename) => {
@@ -45,45 +107,99 @@ const openaiBaseSetings = {
                 file: fs.createReadStream(audioFilename),
                 model: process.env.OPENAI_STT_MODEL,
             })
-            parentPort.postMessage({
+            const message = {
+                role: 'user',
+                content: transcription.text,
+            }
+            messagesPush(message)
+
+            await tasks.sendMessage({
                 from: 'user',
-                message: {
-                    role: 'user',
-                    content: transcription.text,
-                },
+                message,
             })
         },
-        sendChatToOpenAi: async (messages) => {
-            const message = messages[messages.length - 1]
+        sendMessage: async (message) => {
+            const { from, message: { role, content, functionCall } } = message
+
+            let line = ''
+
+            if (functionCall) {
+                if (functions[functionCall.name]) {
+                    functions[functionCall.name].f(functionCall.args)
+                    // write message to file with time of message to europ format
+                    fs.appendFileSync('messages.txt', `${new Date().toLocaleString('fr-FR')}\nfunctionCall: ${functionCall.name}\n\n`)
+                }
+                return
+            }
+
+            if (from.includes('twitchChat')) {
+                const twitchChatUsername = from.split(':')[1]
+                if (!twitchChatUsers.includes(twitchChatUsername)) {
+                    twitchChatUsers.push(twitchChatUsername)
+                }
+
+                line = `(twitch chat) ${twitchChatUsername}: "${content.replace('"', '\'\'')}"`
+            } else {
+                line = `${rolesNames[from] || from}: ${content}`
+            }
+
+            console.log(line)
+
+            // write message to file with time of message to europ format
+            fs.appendFileSync('messages.txt', `${new Date().toLocaleString('fr-FR')}\n${line}\n\n`)
+
+            messagesPush({
+                role,
+                content: line,
+            })
+
+            // if assistant name is in text, ask worker for an openai answer
+            if (role === 'user' && content.includes(`${rolesNames.assistant}`)) {
+                await tasks.sendChatToOpenAi([
+                    ...systemMessages(),
+                    ...messages.map((m) => ({
+                        role: m.role,
+                        content: m.content,
+                    })),
+                ])
+            } else if (role === 'assistant') {
+                await tasks.sendMessageToChat([content])
+            }
+        },
+        sendChatToOpenAi: async (chat) => {
+            const message = chat[chat.length - 1]
 
             let authorisedFunctions = null
 
             // filter functions by role in the processed message
             if (message.content.includes('(twitch chat)')) {
-                authorisedFunctions = functions.filter((f) => f.canRun.includes('twitch chat'))
+                authorisedFunctions = Object.entries(functions)
+                    .filter(([, value]) => value.canRun.includes('twitch chat'))
             } else if (message.role === 'user') {
-                authorisedFunctions = functions.filter((f) => f.canRun.includes('user'))
+                authorisedFunctions = Object.entries(functions)
+                    .filter(([, value]) => value.canRun.includes('user'))
             } else if (message.role === 'assistant') {
-                authorisedFunctions = functions.filter((f) => f.canRun.includes('assistant'))
+                authorisedFunctions = Object.entries(functions)
+                    .filter(([, value]) => value.canRun.includes('assistant'))
             }
 
             const options = {
-                messages,
+                messages: chat,
                 ...openaiBaseSetings,
             }
 
-            if (authorisedFunctions.length) {
-                options.functions = authorisedFunctions.map((f) => ({
-                    name: f.name,
-                    description: f.description,
-                    parameters: f.parameters,
+            if (authorisedFunctions.length > 0) {
+                options.functions = authorisedFunctions.map(([, value]) => ({
+                    name: value.name,
+                    description: value.description,
+                    parameters: value.parameters,
                 }))
             }
 
             // ask openai for an answer
             const answer = await openai.chat.completions.create(options)
 
-            parentPort.postMessage({
+            tasks.sendMessage({
                 from: 'assistant',
                 message: {
                     role: 'assistant',
--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const fs = require('fs')
 const ffmpeg = require('fluent-ffmpeg')
 const mic = require('mic')
-const gptTokenizer = require('gpt-tokenizer')
-
-gptTokenizer.default.modelName = 'cl100k_base'
 
 const { Readable } = require('stream')
 
@@ -14,11 +11,6 @@ const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path
 require('dotenv').config()
 
 ffmpeg.setFfmpegPath(ffmpegPath)
-
-const rolesNames = {
-    user: process.env.PROMPT_USER_NAME,
-    assistant: process.env.PROMPT_ASSISTANT_NAME,
-}
 
 let i = 0
 
@@ -39,7 +31,6 @@ async function recordAudio() {
             exitOnSilence: process.env.MIC_EXIT_ON_SILENCE,
         })
 
-        console.log(`-rec-${i}-start`)
         const micInputStream = micInstance.getAudioStream()
         const output = fs.createWriteStream(filename)
         const writable = new Readable().wrap(micInputStream)
@@ -66,113 +57,11 @@ async function recordAudio() {
     })
 }
 
-const twitchChatUsers = []
-
-const twitchChatUserString = async () => {
-    if (twitchChatUsers.length === 0) return ''
-    if (twitchChatUsers.length === 1) return `L'utilisateur: ${twitchChatUsers[0]} est dans le chat twitch.`
-    return `Les utilisateurs: ${twitchChatUsers.slice(0, -1).join(', ')} et ${twitchChatUsers.slice(-1)} sont dans le chat twitch.`
-}
-
-const systemMessages = () => [
-    {
-        role: 'system',
-        content: `tu es l'assistant, tu te nommes ${rolesNames.assistant} et tu ne réponds qu'en ton nom.
-Le pseudo de l'utilisateur principal est ${rolesNames.user}.
-L'assistant fait des réponses courtes et précises.
-Il y a toujours le nom de l'utilisateur qui s'exprime devant les messages.
-${twitchChatUserString()}`,
-    },
-    {
-        role: 'user',
-        content: `Dit moi ${rolesNames.assistant} comment ça va ?`,
-    },
-    {
-        role: 'assistant',
-        content: `${rolesNames.assistant}: Salut ${rolesNames.user}, ça va très bien, et vous ?`,
-    },
-    {
-        role: 'user',
-        content: `(twitch chat) Xx_dark_sasuke_xX: "Bonjour ${rolesNames.assistant} !"`,
-    },
-    {
-        role: 'assistant',
-        content: `${rolesNames.assistant}: Salut Xx_dark_sasuke_xX, ça va super, et toi ?`,
-    },
-]
-
-const messages = []
-const messagesPush = (message) => {
-    messages.push(message)
-
-    // shift messages until total tokens is less than process.env.OPENAI_API_MAX_CHAT_TOTAL_TOKEN
-    while (gptTokenizer.isWithinTokenLimit(
-        [...systemMessages(), ...messages]
-            .map((m) => `${m.role}: "${m.content}"\n`)
-            .join(''),
-        parseInt(process.env.OPENAI_API_MAX_CHAT_TOTAL_TOKEN, 10),
-    ) === false) {
-        messages.shift()
-    }
-}
-
 const apiWorker = new Worker('./apiWorker.js')
-
-const functions = {
-    addMarker: () => console.log('adding marker'),
-}
-
-apiWorker.on('message', (message) => {
-    const { from, message: { role, content, functionCall } } = message
-
-    let line = ''
-
-    if (functionCall) {
-        if (functions[functionCall.name]) {
-            functions[functionCall.name](...functionCall.args)
-            // write message to file with time of message to europ format
-            fs.appendFileSync('messages.txt', `${new Date().toLocaleString('fr-FR')}\nfunctionCall: ${functionCall.name}\n\n`)
-        }
-        return
-    }
-
-    if (from.includes('twitchChat')) {
-        const twitchChatUsername = from.split(':')[1]
-        if (!twitchChatUsers.includes(twitchChatUsername)) twitchChatUsers.push(twitchChatUsername)
-
-        line = `(twitch chat) ${twitchChatUsername}: "${content.replace('"', '\'\'')}"`
-    } else {
-        line = `${rolesNames[from] || from}: ${content}`
-    }
-
-    // write message to file with time of message to europ format
-    fs.appendFileSync('messages.txt', `${new Date().toLocaleString('fr-FR')}\n${line}\n\n`)
-
-    messagesPush({
-        role,
-        content: line,
-    })
-
-    // if assistant name is in text, ask worker for an openai answer
-    if (role === 'user' && content.includes(`${rolesNames.assistant}`)) {
-        apiWorker.postMessage({
-            f: 'sendChatToOpenAi',
-            args: [[
-                ...systemMessages(),
-                ...messages.map((m) => ({
-                    role: m.role,
-                    content: m.content,
-                })),
-            ]],
-        })
-    } else if (role === 'assistant') {
-        console.log('sending to twitch chat')
-        apiWorker.postMessage({ f: 'sendMessageToChat', args: [content] })
-    }
-})
 
 // main function
 async function main() {
+    console.log(`-rec-${i}-start`)
     // record audio and resolve when it's usable
     const audioFilename = await recordAudio()
 
