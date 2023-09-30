15a5c5edca12705a553cbef374263e730aa8052f
CyrilDenoyelle <Cyril-denoyelle@hotmail.fr>
2023-09-30 16:28:44 +0200

    feat: use discord to receive audio in bot

--- index.js
+++ index.js
@@ -1,74 +1,112 @@
 const fs = require('fs')
-const ffmpeg = require('fluent-ffmpeg')
-const mic = require('mic')
-
-const { Readable } = require('stream')
+const wav = require('wav')
 
 const { Worker } = require('worker_threads')
 
-const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path
+const {
+    Client, GatewayIntentBits, Partials,
+} = require('discord.js')
+const { OpusEncoder } = require('@discordjs/opus')
+
+const {
+    joinVoiceChannel, EndBehaviorType,
+} = require('@discordjs/voice')
 
 require('dotenv').config()
 
-ffmpeg.setFfmpegPath(ffmpegPath)
-
-let i = 0
-
-// Record audio
-async function recordAudio() {
-    return new Promise((resolve, reject) => {
-        let recordTime = 0
-        const filename = `recorded_audios/recorded_audio${i}.wav`
-
-        setInterval(() => {
-            recordTime += 1
-        }, 1000)
-
-        const micInstance = mic({
-            rate: '16000',
-            channels: '1',
-            fileType: 'wav',
-            exitOnSilence: process.env.MIC_EXIT_ON_SILENCE,
-        })
-
-        const micInputStream = micInstance.getAudioStream()
-        const output = fs.createWriteStream(filename)
-        const writable = new Readable().wrap(micInputStream)
-
-        writable.pipe(output)
-
-        micInstance.start()
-
-        micInputStream.on('silence', async () => {
-            micInstance.stop()
-
-            if (recordTime <= process.env.MIC_MINIMUN_RECORD_DURATION) {
-                // too short, retry
-                const retryFileName = await recordAudio()
-                resolve(retryFileName)
-            } else {
-                console.log(`-rec-${i}-stop-${recordTime}`)
-                i += 1
-                resolve(filename)
-            }
-        })
-
-        micInputStream.on('error', reject)
-    })
-}
-
 const apiWorker = new Worker('./apiWorker.js')
 
-// main function
-async function main() {
-    console.log(`-rec-${i}-start`)
-    // record audio and resolve when it's usable
-    const audioFilename = await recordAudio()
+const {
+    MIC_EXIT_ON_SILENCE,
+    MIC_MINIMUN_RECORD_DURATION,
+    DISCORD_GUILD_ID,
+    DISCORD_VOICE_CHANNEL_ID,
+} = process.env
 
-    apiWorker.postMessage({ f: 'sendAudioToOpenAi', args: [audioFilename] })
+const discordClient = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.GuildVoiceStates,
 
-    main()
-}
+    ],
+    partials: [Partials.Channel],
+})
 
-console.log('Recording... Press Ctrl+C to stop.')
-main()
+discordClient.on('ready', () => {
+    // Assuming you have a guild object
+    const guild = discordClient.guilds.cache.get(DISCORD_GUILD_ID)
+
+    // Join the .env voice channel
+    const connection = joinVoiceChannel({
+        channelId: DISCORD_VOICE_CHANNEL_ID,
+        guildId: DISCORD_GUILD_ID,
+        adapterCreator: guild.voiceAdapterCreator,
+        selfDeaf: false,
+        selfMute: false,
+    })
+
+    // Supprime les auditeurs pour utiliser la fonction personnalisée.
+    connection.receiver.speaking.removeAllListeners()
+
+    const talkingUsers = new Set()
+
+    // when user start speaking
+    connection.receiver.speaking.on('start', (userId) => {
+        const timeStart = new Date()
+
+        // get discord user by id
+        const user = discordClient.users.cache.get(userId)
+
+        // avoid multiple start for the same user
+        if (talkingUsers.has(userId)) return
+        talkingUsers.add(userId)
+
+        const subscription = connection.receiver.subscribe(userId, {
+            end: {
+                behavior: EndBehaviorType.AfterSilence,
+                duration: MIC_EXIT_ON_SILENCE,
+            },
+        })
+
+        const audioFileName = `./recorded_audios/${new Date().toISOString().replace(/:/g, '-')}-${user.username}.wav`
+
+        const outputFileStream = new wav.FileWriter(audioFileName, {
+            sampleRate: 48000,
+            bitDepth: 16,
+            channels: 1,
+        })
+
+        const encoder = new OpusEncoder(48000, 1)
+
+        // write chunks of data to the file
+        subscription.on('data', (chunk) => {
+            outputFileStream.write(encoder.decode(chunk))
+        })
+
+        subscription.once('end', async () => {
+            // check if record duration is not too short
+            if (new Date() - MIC_EXIT_ON_SILENCE - timeStart < MIC_MINIMUN_RECORD_DURATION) {
+                talkingUsers.delete(userId)
+
+                // delete the too short output file
+                outputFileStream.end(() => {
+                    fs.unlinkSync(audioFileName)
+                })
+                return
+            }
+
+            // give audio to worker
+            outputFileStream.end(() => {
+                apiWorker.postMessage({ f: 'sendAudioToOpenAi', args: [audioFileName] })
+            })
+
+            // remove user from talkingUsers
+            talkingUsers.delete(userId)
+        })
+    })
+})
+
+discordClient.login(process.env.DISCORD_SECRET)
